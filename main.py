dd8ea07b7ea3e28723577313f9839cc39aa67307
Tabea Heckenthaler <tabea.heckenthaler@weizmann.ac.il>
2022-03-08 09:48:34 +0200

    Added TODO's

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import numpy as np
 
 
 class Human_Solver:
-    def __init__(self, costs, similarities):
+    def __init__(self, real_connections, perceived_connections, costs, similarities):
         self.costs = costs
-        self.graph = nx.Graph()
+        self.graph = nx.Graph(real_connections)
+        self.perception_graph = nx.Graph(perceived_connections)
         self.similarities = similarities
         self.current = 'a'
 
@@ -30,18 +31,29 @@ class Human_Solver:
         :param edge:
         :return: whether traversing edge is successful
         """
+        # TODO: check whether in self.graph these are really two connected nodes, maybe we should add a source of
+        #  randomness as well
         pass
 
     def decrease_cost(self, edge):
+        # TODO: how to decrease the cost
         pass
 
     def increase_cost(self, edge):
+        # TODO: how to increase the cost
         pass
 
 
-# ..set up
-costs = np.ones((N, N))*np.inf
+if __name__ == '__main__':
+    # TODO: Data structure, that can be given to nx.Graph()
+    real_connections = None
+    perceived_connections = None
 
-similarities = dict(pairs)
+    # TODO: Define initial costs.
+    costs = np.ones((N, N))*np.inf
 
-solver = Human_Solver(costs, similarities)
+    # TODO: define dictionary with similar pairs (like in README.txt)
+    similarities = dict(pairs)
+
+    solver = Human_Solver(real_connections, perceived_connections, costs, similarities)
+    solver.path_planning()
